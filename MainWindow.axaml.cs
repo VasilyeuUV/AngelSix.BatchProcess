3c37b288bc34056263e75c4ee096fd50e0cebdd1
VasilyeuUV <vasilyeu.uv@mail.ru>
2025-03-08 02:24:20 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AngelSix.BatchProcess
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
